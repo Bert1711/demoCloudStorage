e3bc6a6d7a65cef8c1df3b2edc5a935f1c1be680
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-18 14:23:12 +0300

    Initial commit

---
+++ src/main/java/ru/zaroyan/democloudstorage/DemoCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.zaroyan.democloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCloudStorageApplication.class, args);
+    }
+
+}
