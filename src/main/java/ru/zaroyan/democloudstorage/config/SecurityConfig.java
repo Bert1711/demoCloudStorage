b4cc22335dea8f9a0d349eb050ecf275ae7d2886
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-21 23:45:58 +0300

    "add SecurityConfig"

---
+++ src/main/java/ru/zaroyan/democloudstorage/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package ru.zaroyan.democloudstorage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import ru.zaroyan.democloudstorage.security.AuthProviderImpl;
+
+/**
+ * @author Zaroyan
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final AuthProviderImpl authProvider;
+
+    @Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider);
+    }
+}
